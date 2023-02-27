6ae3f467f5e56a3a3968196c8eeb1938f1cde829
ZingaG <trad_bar@mail.ru>
2023-02-27 11:50:03 +0300

    HelloSum example added

---
+++ HelloConsole/HelloSum/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input first digit = ");
+
+int a = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Input second digit = ");
+
+int b = Int32.Parse(Console.ReadLine());
+
+int sum = a + b;
+
+System.Console.WriteLine("Sum = " + sum);
+
