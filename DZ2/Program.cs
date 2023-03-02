a6466d027d3b552e6402cd52038a419188d668ac
ZingaG <trad_bar@mail.ru>
2023-03-02 17:24:00 +0300

    Task 1 added

---
+++ DZ2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Task 1
+
+System.Console.WriteLine("Input a = ");
+int a = Int32.Parse(Console.ReadLine());
+int res = 0;
+
+for (int i = 0; i < 2; i++)
+{
+    if (i == 1)
+    {
+        res = a % 10;
+    }
+    a = a / 10;
+}
+
+System.Console.WriteLine("Result = " + res);
--- HelloConsole/HelloDiv/Program.cs
+++ HelloConsole/HelloDiv/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Input first digit = ");
 
-int a = Int32.Parse(Console.ReadLine());
+double a = Int32.Parse(Console.ReadLine());
 
 Console.WriteLine("Input second digit = ");
 
-int b = Int32.Parse(Console.ReadLine());
+double b = Int32.Parse(Console.ReadLine());
 
 double divison = a / b;
 
